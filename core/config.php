a96576f82a343f7a6f68faf30deed3d3f17e994c
eqlipt <squish@list.ru>
2022-04-19 10:08:57 +0300

    config separate file

---
+++ core/config.php
@@ -0,0 +1,6 @@
+<?php
+// Price constants
+define("PRICE_DELIVERY_THRESHOLD_CITY", '3 000');
+define("PRICE_DELIVERY_THRESHOLD_SUBURBS", '6 000');
+
+?>
--- core/initialize.php
+++ core/initialize.php
@@ -11,14 +11,11 @@ define("WWW_CSS", '/css');
 define("WWW_JS", '/js');
 define("WWW_IMG", '/img');
 
-require_once('functions.php'); 
+require_once('config.php');
+require_once('functions.php');
 require_once('database.php');
 require_once('query_functions.php');
 require_once('validation_functions.php');
-
-// Price constants
-define("PRICE_DELIVERY_THRESHOLD_CITY", '3 000');
-define("PRICE_DELIVERY_THRESHOLD_SUBURBS", '6 000');
 
 
 $db = db_connect();
