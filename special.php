fd1d19b2b13b3dd8ce0999a15f252ac68891e81a
eqlipt <squish@list.ru>
2022-04-26 11:26:33 +0300

    из комплектов убран фуршет на природе

--- special.php
+++ special.php
@@ -68,23 +68,23 @@ include(TPL_PATH . '/header.php');
         <p class="set-description">Всё необходимое снаряжение для одного человека: компактная палатка, комфортный самонадувающийся коврик и спальник. Фонарик можно подвесить под потолком палатки или установить на полу.</p>
     </div>
 
-    <div class="set-item card">
-        <img class="box saturate" alt="Комплект Фуршет на природе" src="<?php echo url_for(WWW_IMG . '/inventory/set/diner.jpg'); ?>">
+    <!--div class="set-item card">
+        <img class="box saturate" alt="Комплект Фуршет на природе" src="<?php #echo url_for(WWW_IMG . '/inventory/set/diner.jpg'); ?>">
         <div class="">
             <h2>"Фуршет на природе" - Комплект для любой компании<a id="diner"></a></h2>
             <ul>
-                <li><a href="<?php echo WWW_ROOT . '/shatry-i-tenty/prokat-shatra-sorang/'; ?>">Шатёр полуавтомат</a></li>
-                <li><a href="<?php echo WWW_ROOT . '/poleznie-veshi/table/'; ?>">Складной стол и 4 табурета</a></li>
-                <li><a href="<?php echo WWW_ROOT . '/poleznie-veshi/holod/'; ?>">Походный холодильник с охлаждающим брикетом</a></li>
-                <li><a href="<?php echo WWW_ROOT . '/poleznie-veshi/prokat-trenogi-s-kotelkom/'; ?>">Тренога с котелком</a></li>
+                <li><a href="<?php #echo WWW_ROOT . '/shatry-i-tenty/prokat-shatra-sorang/'; ?>">Шатёр полуавтомат</a></li>
+                <li><a href="<?php #echo WWW_ROOT . '/poleznie-veshi/table/'; ?>">Складной стол и 4 табурета</a></li>
+                <li><a href="<?php #echo WWW_ROOT . '/poleznie-veshi/holod/'; ?>">Походный холодильник с охлаждающим брикетом</a></li>
+                <li><a href="<?php #echo WWW_ROOT . '/poleznie-veshi/prokat-trenogi-s-kotelkom/'; ?>">Тренога с котелком</a></li>
             </ul>
             <ul class="price">
                 <li><p>первые двое суток:  4 400 ₽</p></li>
                 <li><p>последующие сутки:  1 100 ₽/сут.</p></li>
-                <li><p>залог: 15 000 ₽ без <a href="<?php echo WWW_ROOT . '/howto.php?question=deposit'; ?>">документа</a> / 1 500 ₽ с <a href="<?php echo WWW_ROOT . '/howto.php?question=deposit'; ?>">документом</a></p></li>
+                <li><p>залог: 15 000 ₽ без <a href="<?php #echo WWW_ROOT . '/howto.php?question=deposit'; ?>">документа</a> / 1 500 ₽ с <a href="<?php #echo WWW_ROOT . '/howto.php?question=deposit'; ?>">документом</a></p></li>
             </ul>
         </div>
         <p class="set-description">Дополнение к любому комплекту. Шатёр комфортно вмещает стол с табуретами и 4-6 человек. Москитные сетки защищают от насекомых. Походный холодильник с охлаждающим брикетом сохраняет охлаждёнными продукты и напитки. В котелке можно подогреть воду для чая, приготовить уху на костре.</p>
-    </div>
+    </div-->
 
 <?php include(TPL_PATH . '/footer.php'); ?>
