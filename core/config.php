4509baff4359bd88f8674abd5dd1978fa1a37748
eqlipt <squish@list.ru>
2022-08-28 22:35:20 +0300

    working hours Kupchino

--- core/config.php
+++ core/config.php
@@ -4,7 +4,7 @@ define("PRICE_DELIVERY_THRESHOLD_CITY", '3 000');
 define("PRICE_DELIVERY_THRESHOLD_SUBURBS", '6 000');
 
 // Contact info
-define("CONTACTS_WORKING_HOURS_KUPCHINO", 'с 9 до 21, суббота - с 9 до 12. Выходные дни: вторник, среда.');
+define("CONTACTS_WORKING_HOURS_KUPCHINO", 'с 8 до 20, суббота - с 7 до 9. Выходные дни: вторник, среда.');
 define("CONTACTS_ADDRESS_CITY", 'Санкт-Петербург');
 define("CONTACTS_ADDRESS_STREET_KUPCHINO", 'Витебский пр. 99 корп. 2');
 define("CONTACTS_ADDRESS_STREET_KOMEND", 'Комендантский пр. 51 корп. 1');
