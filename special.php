4a823705f7bd197f3b47743d73c064616a55e1c5
eqlipt <squish@list.ru>
2023-06-14 17:20:42 +0300

    стоимость комплекта для 2

--- special.php
+++ special.php
@@ -42,7 +42,7 @@ include(TPL_PATH . '/header.php');
             </ul>
             <ul class="price">
                 <li><p>первые двое суток: 3 980 ₽</p></li>
-                <li><p>последующие сутки: 950 ₽/сут.</p></li>
+                <li><p>последующие сутки: 710 ₽/сут.</p></li>
                 <li><p>залог: 20 000 ₽ без <a href="<?php echo WWW_ROOT . '/howto.php?question=deposit'; ?>">документа</a> / 2 000 ₽ с <a href="<?php echo WWW_ROOT . '/howto.php?question=deposit'; ?>">документом</a></p></li>
             </ul>
         </div>
