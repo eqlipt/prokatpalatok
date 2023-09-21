d7a3e860fc10b65b676c0311b03d12da096e3d86
eqlipt <squish@list.ru>
2023-09-21 10:26:35 +0300

    change links in left bar tamplate from /special.php to /pohodnye-komplekty/

--- tpl/left.php
+++ tpl/left.php
@@ -1,25 +1,25 @@
 <!-- left -->
 <section id="left">
-    <a href="<?php echo WWW_ROOT . '/special.php'; ?>">
+    <a href="<?php echo WWW_ROOT . '/pohodnye-komplekty/'; ?>">
         <h3>Комплекты</h3>
     </a>
 
     <div class="side-card">
-        <a href="<?php echo WWW_ROOT . '/special.php#big_family'; ?>">
+        <a href="<?php echo WWW_ROOT . '/pohodnye-komplekty/#big_family'; ?>">
             <img class="box saturate" alt="Походный комплект Большая семья" src="<?php echo url_for(WWW_IMG . '/inventory/set/big_family_160.jpg'); ?>"/>
             <p>Большая семья</p>
         </a>
     </div>
 
     <div class="side-card">
-        <a href="<?php echo WWW_ROOT . '/special.php#for_two'; ?>">
+        <a href="<?php echo WWW_ROOT . '/pohodnye-komplekty/#for_two'; ?>">
             <img class="box saturate" alt="Походный комплект Для двоих" src="<?php echo url_for(WWW_IMG . '/inventory/set/for_two_160.jpg'); ?>"/>
             <p>Для двоих</p>
         </a>
     </div>
 
     <div class="side-card">
-        <a href="<?php echo WWW_ROOT . '/special.php#solo'; ?>">
+        <a href="<?php echo WWW_ROOT . '/pohodnye-komplekty/#solo'; ?>">
             <img class="box saturate" alt="Походный комплект для одного" src="<?php echo url_for(WWW_IMG . '/inventory/set/solo_160.jpg'); ?>"/>
             <p>Соло</p>
         </a>
