6c5bf4d3dcc802c181cbc22d67430286d290f50c
eqlipt <squish@list.ru>
2023-10-04 18:11:14 +0300

    clear redundant content from /*.php 301 pages

--- delivery.php
+++ delivery.php
@@ -2,44 +2,5 @@
 
 Header("HTTP/1.1 301 Moved Permanently");
 Header("Location: https://prokatpalatok.ru/dostavka/");
-
-/* 
-require_once('../core/initialize.php'); 
-$page_title = "Доставка снаряжения: Прокат палаток в Санкт-Петербурге";
-$page_keywords = "прокат палаток доставка, доставка снаряжения спб";
-$page_description = "Прокат палаток: стоимость доставки";
-$page_breadcrumbs = '<a href="' . WWW_ROOT . '/usloviya-prokata/' . '">Как арендовать</a>' . ' > Доставка';
-$page_content_class = "infopage";
-include(TPL_PATH . '/header.php');
-include(TPL_PATH . '/left.php');
-
+exit();
 ?>
-
-<!-- center -->
-<section id="center">
-    <article>
-        <h1>Стоимость доставки снаряжения в Санкт-Петербурге и Ленобласти</h1>
-        <div class="card text-block padding-block">
-            <p>Московский, Фрунзенский,  Кировский: 600 - 800 ₽*</p>
-        </div>
-
-        <div class="card text-block padding-block">
-            <p>Адмиралтейский, Центральный, Петроградский, Василеостровский, Невский, Красносельский, Пушкинский: 800 - 1 000 ₽*</p>
-        </div>
-
-        <div class="card text-block padding-block">
-            <p>Выборгский, Калининский, Красногвардейский, Приморский, Павловский, Колпинский: 1 000 - 1 800 ₽*</p>
-        </div>
-
-        <div class="card text-block padding-block">
-            <p>Доставка за пределами КАД: 800 ₽ + 50 ₽/км**</p>
-        </div>
-        <br>
-        <p class="sub light-yellow">* Доставка осуществляется при заказе от <?php echo PRICE_DELIVERY_THRESHOLD_CITY; ?> ₽. Сумма варьируется в пределах района.</p>
-        <p class="sub light-yellow">** Доставка осуществляется при заказе от <?php echo PRICE_DELIVERY_THRESHOLD_SUBURBS; ?> ₽.</p>
-    </article>
-</section>
-<!-- /center -->
-
-<?php include(TPL_PATH . '/right.php'); ?>
-<?php include(TPL_PATH . '/footer.php'); */ ?>
