3eb999b0cd403d53cc0bc9ba88bd05c62862e133
eqlipt <squish@list.ru>
2023-11-15 07:04:18 +0300

    Clarify script goal

--- js/slider.js
+++ js/slider.js
@@ -1,6 +1,10 @@
+// Rotate images in the top banner section
 const NumberOfImagesToRotate = 45;
 FirstPart = ' url("https://prokatpalatok.ru/img/title/title';
 LastPart = '.jpg")';
 var r = Math.ceil(Math.random() * NumberOfImagesToRotate);
 var image = FirstPart + r + LastPart;
-document.getElementById("slider").style.background = 'linear-gradient(rgba(40,40,40,0.65),rgba(200,200,200,0.35)),' + image + ' center/cover no-repeat';
+document.getElementById("slider").style.background =
+  "linear-gradient(rgba(40,40,40,0.65),rgba(200,200,200,0.35))," +
+  image +
+  " center/cover no-repeat";
