4ccc9088fa973ac4ade88e73f79647b2da7a76b2
eqlipt <squish@list.ru>
2023-11-16 14:15:09 +0300

    added fading effect to images appearing after load on inventory pages

--- js/spinner.js
+++ js/spinner.js
@@ -5,8 +5,12 @@ imagePlaceholderArray.forEach((item, id) => {
   imageToAppend.src = item.href;
   imageToAppend.alt = item.dataset.alt;
   imageToAppend.classList.add("box");
+  imageToAppend.classList.add("fadeout");
   imageToAppend.addEventListener("load", function () {
-    imagePlaceholderArray[id].appendChild(imageToAppend);
     item.classList.remove("spinner");
+    imagePlaceholderArray[id].appendChild(imageToAppend);
+    setTimeout(function () {
+      imageToAppend.classList.add("fadein");
+    }, 250);
   });
 });
