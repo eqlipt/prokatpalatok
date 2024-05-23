66854242ac6a5ab96c566d5c3f3b23eea556536b
eqlipt <squish@list.ru>
2024-05-23 10:44:35 +0300

    new sleeper treck

---
+++ spalniki/prokat-tyoplogo-spalnika-kokona-treck/index.php
@@ -0,0 +1,12 @@
+<?php
+
+require_once('../../core/initialize.php');
+$inventory_id='26';
+$page_content_class = "infopage";
+
+include(TPL_PATH . '/header.php');
+include(TPL_PATH . '/left.php');
+include(TPL_PATH . '/inventory.php');
+include(TPL_PATH . '/right.php');
+include(TPL_PATH . '/footer.php');
+?>
